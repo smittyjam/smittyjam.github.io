ecf60e6e7cb6b48e64445e1d64c8cde0cdb60102
superstreem <ryan.pieszko@outlook.com>
2020-11-29 12:23:06 +1000

    Update fbAPI.js

--- fbAPI.js
+++ fbAPI.js
@@ -1,1 +1,8 @@
-
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '605002377072519',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v9.0'
+    });
+  };
