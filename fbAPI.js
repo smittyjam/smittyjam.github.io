205764d38e3fb9b03a1fa928a927039abfe07abf
superstreem <ryan.pieszko@outlook.com>
2020-11-29 11:58:27 +1000

    1

--- fbAPI.js
+++ fbAPI.js
@@ -1,3 +1,16 @@
-FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-});
+ window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '605002377072519',
+      xfbml      : true,
+      version    : 'v9.0'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
