017fe0766cc0067bd79e8907124e9e7cdf3825dd
superstreem <ryan.pieszko@outlook.com>
2020-12-13 12:28:05 +1000

    j

--- fbAPI.js
+++
@@ -1,8 +0,0 @@
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId            : '605002377072519',
-      autoLogAppEvents : true,
-      xfbml            : true,
-      version          : 'v9.0'
-    });
-  };
